3812c626b515f02b80bae5d0ba5309285140e100
Jeewon Kim <jki00129@gmail.com>
2018-03-12 01:42:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
+var myString = `1. ${dwarves[0]} `
+		for (let i = 1; i< dwarves.length; i++) {
+			myString += `${i+1}. ${dwarves[i]} `
+		}
+		return myString
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  for (let i = 0; i< planeteerCalls.length; i++)
+planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!"
+  return planeteerCalls
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i< words.length; i++) {
+    if (words[i].length > 4) {
+      return true
+    }
+  }
+  return false
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return foods[i]
+    }
+  }
+  return "no cheese!"
 }
