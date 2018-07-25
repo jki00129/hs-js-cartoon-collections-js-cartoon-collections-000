85b2c28a71a5197b787451b5a6c3fd5643be4c70
Jeewon Kim <jki00129@gmail.com>
2018-07-25 12:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-function dwarfRollCall(dwarves) {
-var myString = `1. ${dwarves[0]} `
-		for (let i = 1; i< dwarves.length; i++) {
-			myString += `${i+1}. ${dwarves[i]} `
-		}
-		return myString
+function dwarfRollCall(dwarves){
+  let myString = `1. ${dwarves[0]} `
+  
+  for (let i =1; i<dwarves.length; i++){
+    myString += `${i+1}. ${dwarves[i]} `
+  }
+  return myString;
 }
 
-function summonCaptainPlanet(planeteerCalls){
-  for (let i = 0; i< planeteerCalls.length; i++)
-planeteerCalls[i] = planeteerCalls[i].toUpperCase() + "!"
+function summonCaptainPlanet (planeteerCalls){
+
+  for(let i = 0; i<planeteerCalls.length; i++) {
+    planeteerCalls[i]= planeteerCalls[i].toUpperCase() + "!"
+}
   return planeteerCalls
 }
 
-function longPlaneteerCalls(words) {
-  for (let i = 0; i< words.length; i++) {
-    if (words[i].length > 4) {
-      return true
+function longPlaneteerCalls(array){
+  for(let i = 0; i<array.length; i++){
+    if(array[i].length > 4){
+      return true;
     }
   }
-  return false
-}
+  return false;
+} 
 
-function findTheCheese (foods) {
-  for (let i = 0; i < foods.length; i++) {
-    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
-      return foods[i]
+function findTheCheese(array){
+  let cheese= ["gouda","cheddar", "camembert"]
+  
+  for(let i = 0; i<array.length; i++){
+    let cheeseIdx = cheese.indexOf(array[i]);
+    if(cheeseIdx !== -1){
+      return array[i]
     }
   }
-  return "no cheese!"
+  return 'no cheese!'
 }
